4fb99ce30782754c86b1cfca42cac62d0099202b
Gregory Terryn <171694387+PrometheusCS@users.noreply.github.com>
2024-07-07 11:10:56 +0200

    Exercice 03_Reversestring OK

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const chars = string.split("");
+    const revers = chars.reverse();
+    const invertString = revers.join("");
+    return invertString;
 };
 
 // Do not edit below this line
